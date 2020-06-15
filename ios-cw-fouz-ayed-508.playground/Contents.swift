119354974fff6dace3e1c0624d2959deecc6f42f
fouz ayed <66705147+fouzm70@users.noreply.github.com>
2020-06-16 02:23:00 +0300

    done

---
+++ ios-cw-fouz-ayed-508.playground/Contents.swift
@@ -0,0 +1,52 @@
+import UIKit
+
+let name = "Fouz ayed"
+
+var age = 16
+var height = 160
+var hobby1 = "painting"
+var hobby2 = "photoghraphing"
+
+print("my Name is: ", name, ", and my age is \(age), and my height is: \(height), my hobbies are: \(hobby1), \(hobby2)")
+ 
+
+// Part2
+var friendName = "Wrood"
+var friendage = 17
+var friendheight = 160
+var friendhobby1 = "photoghraphing"
+var friendhobby2 = "sleeping"
+
+
+name < friendName
+age < friendage
+hobby1 < friendhobby1
+hobby2 < friendhobby2
+height < friendheight
+
+
+age > friendage
+height > friendheight
+
+
+age <= friendage
+height <= friendheight
+
+
+age >= friendage
+height >= friendheight
+
+name == friendName
+age == friendage
+hobby1 == friendhobby1
+hobby2 == friendhobby2
+height == friendheight
+
+name != friendName
+age != friendage
+hobby1 != friendhobby1
+hobby2 != friendhobby2
+height != friendheight
+
+print("my age is: \(age), my friendage is: \(friendage) ")
+
